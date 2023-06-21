91d17de53e794ac30b952c4c2c8e640776ef56e6
Adnanarodiya <aiarodiya07@gmail.com>
2023-06-21 08:58:52 +0530

    Co-authored-by: Bhavesh Patil <Uraniumking007@users.noreply.github.com> Co-authored-by: Bhumil Sojitra <Bhumil-Sojitra@users.noreply.github.com>

--- src/script.js
+++ src/script.js
@@ -2,17 +2,18 @@ let Amount = document.getElementById('amount');
 let category = document.getElementById('Category');
 let Add = document.getElementById('add');
 let spentList = document.querySelector('#spent-list');
-let spentItem = document.querySelector('.spent-item');
+const spentItem = document.querySelector('.spent-item');
 
-console.log(spentItem);
+// console.log(spentItem);
 
-spentItem = () => {
-  spentItem.classList.remove('hidden');
+const addItem = () => {
+  const div = document.createElement('div')
+  div.className = 'w-2/4 flex items-center p-8 text-center h-9 mt-6 bg-slate-400 rounded-lg appearance-none'
+  div.id = 'spent-list'
+  spentItem.appendChild(div)
+  div.textContent = `Amount = ${Amount.value} Category = ${category.value}`
 };
 
 Add.addEventListener('click', () => {
-  // console.log(spentList.innerHTML);
-  // console.log(Amount.value, category.value);
-  spentList.innerHTML = `Amount = ${Amount.value} Category = ${category.value}`;
-  spentItem();
+  addItem();
 });
